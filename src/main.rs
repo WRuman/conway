1330fad0da3cdcea70178efbcbefd9ef9e497753
Will Ruman <will.ruman@penton.com>
2017-04-27 19:05:22 -0500

    Speeds things up a bit

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let sleep_time = Duration::from_millis(350);
+    let sleep_time = Duration::from_millis(100);
     let mut sim = Game::new_with_size(50);
     sim.add_glider();
     sim.show_board();
